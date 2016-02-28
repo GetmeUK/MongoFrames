e6a09f6adefdf04601e7e2dea3630815db9d366a
Anthony Blackshaw <ant@getme.co.uk>
2016-02-28 00:30:53 +0000

    Added IndexModel to the pymongo automatic imports

--- mongoframes/__init__.py
+++ mongoframes/__init__.py
@@ -2,6 +2,7 @@ from mongoframes.frames import *
 from mongoframes.pagination import *
 from mongoframes.queries import *
 from pymongo import (
+    IndexModel,
     ASCENDING as ASC,
     DESCENDING as DESC
 )
