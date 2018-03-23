4939bb1ac3d8ac97fc1cddcd37630d3b85c075b1
Anthony Blackshaw <ant@getme.co.uk>
2018-03-23 14:16:26 +0000

    Added additional pymongo pass-thoughs in the base __init__ for convenience: Collation, GEOSPHERE, TEXT

--- mongoframes/__init__.py
+++ mongoframes/__init__.py
@@ -1,8 +1,11 @@
 from mongoframes.frames import *
 from mongoframes.pagination import *
 from mongoframes.queries import *
+from pymongo.collation import Collation
 from pymongo import (
     IndexModel,
     ASCENDING as ASC,
-    DESCENDING as DESC
+    DESCENDING as DESC,
+    GEOSPHERE,
+    TEXT
 )
